dbf27b77bdfa59d0f048602dd14b9251f63d0d3f
Tim Rediehs <timthetic@Tims-MacBook-Pro.local>
2017-10-25 19:57:16 -0700

    Me trying to do something, but failing

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
+var cats = [];
+var s1 = "resources/";
+var s2 = ".jpeg";
+var slectedCatID = 13;
+for (i = 1; i < 13; i++) { 
+
+    cats[i-1] = s1.concat(i.toString().concat(s2));
+}
+console.log(cats);
+
+
+
 function attachEventHandlers() {
 	$('.image').on('click', handleImageClick);
 }
@@ -8,6 +20,7 @@ function handleImageClick(event) {
 	const images = $('.photo-row .image');
 	const info = $('.info-pane');
 	const preview = $('.preview-image');
+	console.log($(event.target))
 
 	if (target.hasClass('selected')) {
 		images.removeClass('not-selected');
@@ -25,6 +38,16 @@ function handleImageClick(event) {
 		preview.css('background-image', target.css('background-image'));
 	}
 }
+function changePicture(){
+	let selectedCat = $('.selected').attr("style");
+	let trimSelectedCat = selectedCat.subtring(22,37);
+
+	console.log(trimSelectedCat);
+}
+setInterval(changePicture, 5000);
+
+
+
 
 $('document').ready(function() {
 
